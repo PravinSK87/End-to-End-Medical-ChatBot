165e8f7202576eff7148d3e4857694624ee5586d
PravinSK <pravinsk87@gmail.com>
2024-06-20 12:33:34 +0530

    Setp file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'Medical ChatBot',
+    version='0.0.0',
+    author='Pravin',
+    author_email='Pravin@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
